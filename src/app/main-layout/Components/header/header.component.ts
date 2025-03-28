edd49863bb1788659a2103b2ec245cd17229d787
abdulrehmantuta <abdulrehmantutal@gmail.com>
2025-03-28 15:53:01 -0700

    New Changes By Abdul

--- src/app/main-layout/Components/header/header.component.ts
+++ src/app/main-layout/Components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SaleService } from '../../Services/sale.service';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,12 @@ import { Router } from '@angular/router';
 export class HeaderComponent {
   constructor(
     private _Route: Router,
+    private _SaleService: SaleService,
   ) {
   }
   onLogout(){
     this._Route.navigate(['/sign-in']);
+    this._SaleService.setSubmittingState(false)
     localStorage.removeItem('token');
   }
 }
--- src/app/main-layout/Services/interceptors/auth.service.ts
+++ src/app/main-layout/Services/interceptors/auth.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { SaleService } from '../sale.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private _SaleService: SaleService,
+  ) {}
 
   canActivate(): boolean {
     const token = localStorage.getItem('token');
+    if(token){
+      this._SaleService.setSubmittingState(true)
+    }
 
     if (!token) {
       this.router.navigate(['/sign-in']);
